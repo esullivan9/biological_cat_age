226ce9e134a65166c0e33c14dc49547c93090f92
Eric Sullivan <esllvn@live.com>
2019-10-23 00:11:47 -0400

    Cat Age Conversion implemented in Java.

---
+++ CatAge_java.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CatAge_java
+{
+    public static void main(String args[])
+    {
+        //Scanner
+        Scanner in = new Scanner(System.in);
+
+        //Ask the user for their cat's age
+        System.out.print("Enter the age of your cat, in years: ");
+        double input_age = in.nextDouble();
+
+        System.out.println("Your cat is " + ageConvert(input_age) + " years old.");
+        
+        in.close();
+    }
+
+    public static double ageConvert(double in_age)
+    {
+        if((in_age >= 0.0) && (in_age <= 1.0))
+            return 16.6364 * in_age;
+        else if((in_age > 1) && (in_age < 2))
+            return 6.6364 * age + 10;
+        else if(in_age >= 2)
+            return 4.1364 * (in_age + 3.62634);
+        else
+            return 0;
+    }
+}
